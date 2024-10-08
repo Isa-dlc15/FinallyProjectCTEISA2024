83491e8e00233f2e077116d89b7f398fd5144e26
Isa.dlc <deleonisaias13@gmail.com>
2024-10-08 16:46:09 -0600

    Finally Project CTEISA

---
+++ FinallyProject2024CTEISA/src/main/data.js
@@ -0,0 +1,9 @@
+export const words = [
+    "hola", "mundo", "programación", "teclado", "mecanografía",
+    "practicar", "escritura", "rapidez", "precisión", "desarrollo",
+    "javascript", "html", "css", "interactivo", "juego", "diversión",
+    "prueba", "desafío", "competencia", "habilidad", "técnica",
+    "algoritmo", "función", "variable", "bucle", "condición", 
+    "proyecto", "estilo", "estructura", "contenido", "modular"
+  ];
+  
